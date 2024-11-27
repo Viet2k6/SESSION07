b505e8f6249336aa4ca6981638842db6a7015f23
Đăng Việt <trandangviet00@gmail.com>
2024-11-27 23:19:27 +0700

    Add files via upload

---
+++ ss7-b4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int size,i;
+	printf("Nhap so phan tu cua mang: ");
+	scanf("%d", &size);
+	int array[size];
+		for(i=0;i<size;i++){
+	printf("Moi nhap phan tu: ");
+	scanf("%d",&array[i]);
+}
+	return 0;
+}
